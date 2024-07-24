b59e55571e27a2ba938b6ba45545b2c9b431d5e4
Jheels Patel <patej162.316@gmail.com>
2024-07-24 18:38:57 +0100

    feat: Updated login and register styling for consistency. - Renamed register.js -> auth.js. - Changed styling for login page. - Updated styling for checkboxes on authentication pages. - Added autocomplete features from browsers.

--- sentify/website/static/scripts/auth.js
+++ sentify/website/static/scripts/auth.js
@@ -10,11 +10,11 @@ function togglePasswordVisibility() {
   document.addEventListener('DOMContentLoaded', function() {
     const password = document.getElementsByName("password")[0];
     const confirm_password = document.getElementsByName("confirm_password")[0];
-    const password_match = document.getElementById("password_match");
+    const passwordMatch = document.getElementById("passwordMatch");
     const passwordCriteria = document.getElementById("passwordCriteria");
   
     function validatePassword() {
-      password_match.style.display = (password.value !== confirm_password.value && confirm_password.value.length > 0) ? "flex" : "none";
+      passwordMatch.style.display = (password.value !== confirm_password.value && confirm_password.value.length > 0) ? "flex" : "none";
     }
   
     function updatePasswordCriteria() {
@@ -23,12 +23,13 @@ function togglePasswordVisibility() {
         'lowercaseCheck': /[a-z]/.test(password.value),
         'uppercaseCheck': /[A-Z]/.test(password.value),
         'numberCheck': /\d/.test(password.value),
-        'specialCheck': /[!@#$%^&*(),.?":{}|<>]/.test(password.value)
+        'specialCheck': /[-’/`~!#*$@_%+=.,^&(){}[\]|;:”<>?\\]/.test(password.value)
       };
       Object.keys(criteriaChecks).forEach(criteria => updateCriteria(criteria, criteriaChecks[criteria]));
     }
-  
+
     function updateCriteria(criteria, isValid) {
+      console.log(password.value)
       const element = document.getElementById(criteria);
       element.classList.toggle("valid", isValid);
       element.classList.toggle("invalid", !isValid);
